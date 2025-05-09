5cee65d389cce6fbdd226161e116bfd7179dbb9e
KewKub <patipatsasothorn@gmail.com>
2025-05-09 18:59:57 +0700

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace freelans.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
